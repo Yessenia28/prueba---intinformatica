558ed40f301b3115fa5fcc12d6315850ca2336d2
Jonatan Hamlet Munguia Martinez <hunterhamlet@gmail.com>
2023-08-07 20:20:02 -0600

    Create a conflict

--- test1.java
+++ test1.java
@@ -5,9 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo");
-		
-		
-
+		System.out.println("Esto es para crear un conflicto");
 	}
 
 }
