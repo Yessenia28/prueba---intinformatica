4e6736b41040d458a2b3ee7b1db2c3d107d9791e
Yessenia28 <yesebr24@gmail.com>
2023-08-07 21:31:01 -0500

    añadiendo cambios

--- test1.java
+++ test1.java
@@ -3,10 +3,8 @@ package main;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hola Mundo");
-		
-		
+
+		System.out.println("Realizando casmbios");
 
 	}
 
