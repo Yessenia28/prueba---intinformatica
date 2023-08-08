f5a2d42f2f0baa9e3201c1a56539a23caa16574c
Yessenia28 <yesebr24@gmail.com>
2023-08-07 21:39:09 -0500

    Generando conflictos

--- test1.java
+++ test1.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 
 		System.out.println("Realizando casmbios");
-
+		System.out.println("Realizando casmbios para generar conficltos ");
 	}
 
 }
